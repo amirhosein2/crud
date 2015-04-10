6a3bf530794e71e8ae6cd23030d620e74cae1a6e
amirhosein2 <sosin@debian>
2015-04-10 16:00:03 -0700

    last commit

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,5 +13,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
+    # Not logged in so a "Login" link will be present
+    assert_select "a[href=?]", login_path
+    # Login
+    log_in_as(@user)
+    # Go to root
+    get root_path
+    # Now logged in so a "Login" link won't be present
+    assert_select "a[href=?]", login_path, count: 0
+    # Now logged in so a "Logout" link will be present
+    assert_select "a[href=?]", logout_path
+    # Now logged in so a "Users" link will be present
+    assert_select "a[href=?]", users_path
+    # Now logged in so a "Accounts" link will be present
+    #assert_select 'a[href=?]', '#', text: 'Accounts'
   end
 end
